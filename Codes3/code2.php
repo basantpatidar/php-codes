e1ac4a99c1f63d79e3bb221eda2340aa407ef9b4
basantpatidar <basantpatidar33@gmail.com>
2018-07-14 20:28:34 -0400

    Variable assignment

---
+++ Codes3/code2.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+ <title>My Second Program: Variable Assignment</title>
+</head>
+<body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: basant
+ * Date: 2/23/2018
+ * Time: 1:31 AM
+ */
+ $string1 = 'First String assigned to a variable called $String1';
+ $string2 = 'Second String assigned to a variable called $String2';
+ echo "<h1>".$string1."</h1>";
+ echo "<h2>".$string2."</h2>";
+?>
+</body>
+</html>
