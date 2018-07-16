fd950bbbe9ea09bf10e7d6253af4580cfdbe32d7
basantpatidar <basantpatidar33@gmail.com>
2018-07-15 20:17:10 -0400

    using different types of operators

---
+++ Codes3/code4.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <title>Different Operators Demo</title>
+</head>
+<body>
+<?php
+
+$number1 = '5';
+$number2 = '10';
+$number3 = '2';
+$number4 = '8';
+echo "<h3>I am demonstrating different arithmetic functions</h3>";
+echo "Addition : ". ($number1 + $number2 + $number3 + $number4) ."<br><br>";
+echo "Subtraction : ". ($number1 - $number2 - $number4) ."<br><br>";
+echo "Division : ". ($number1 / $number2 / $number3) ."<br><br>";
+echo "Multiplication : " .($number1 * $number2 * $number4 * $number3) ."<br><br>";
+?>
+</body>
+</html>
