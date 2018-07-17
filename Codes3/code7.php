a3089700b7e359df4031cc0a2614a2fae52cec0d
basantpatidar <basantpatidar33@gmail.com>
2018-07-17 01:16:41 -0400

    Use of array

---
+++ Codes3/code7.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: basant
+ * Date: 2/23/2018
+ * Time: 12:30 PM
+ */
+$color = array('white','green','red','blue','black');
+print "The memory of that scene for me is like a frame of film forever frozen at that
+moment: the ". $color[2]." carpet, the ". $color[1]. "lawn, the ". $color[0]. "house, the leaden sky. The
+new president and his first lady. - Richard M. Nixon";
