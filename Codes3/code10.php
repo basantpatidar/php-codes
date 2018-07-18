a27b1607c134bed554c1b72d575599837782de1d
basantpatidar <basantpatidar33@gmail.com>
2018-07-18 01:39:40 -0400

    use of for loop

---
+++ Codes3/code10.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: basant
+ * Date: 2/23/2018
+ * Time: 2:00 PM
+ */
+for($i = 1; $i <=10; $i++){
+    print ($i);
+    if($i<10){
+        print"-";
+    }
+}
