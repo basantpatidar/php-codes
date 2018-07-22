7ecf34960c88bd40fb46c86c241e48813a2bee63
basantpatidar <basantpatidar33@gmail.com>
2018-07-21 23:54:37 -0400

    Code 12- Associative Array with for-loop

---
+++ Codes3/code12.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: basant
+ * Date: 2/23/2018
+ * Time: 3:15 PM
+ */
+$name = array("Jane Smith"=>"31","Jacob Smith"=>"41","William
+Smith"=>"39","Frodo Baggins"=>"40");
+asort($name);
+print"Using ascending order sort by value";
+echo "<br>";
+foreach($name as $i => $i_value) {
+    echo $i . " ".$i_value;
+    echo "<br>";
+}
+ksort($name);
+
+print"Using ascending order sort by key";
+echo "<br>";
+foreach($name as $i => $i_value) {
+    echo $i . " ".$i_value;
+    echo "<br>";
+}
+arsort($name);
+print"Using descending order sort by value";
+echo "<br>";
+foreach($name as $i => $i_value) {
+    echo $i . " ".$i_value;
+    echo "<br>";
+}
+arsort($name);
+print"Using descending order sort by key";
+echo "<br>";
+foreach($name as $i => $i_value) {
+    echo $i . " ".$i_value;
+    echo "<br>";
+}
+    ?>
