d213642794c7ac8a629e3b204d94d0356d9ba6d4
basantpatidar <basantpatidar33@gmail.com>
2018-07-27 20:38:43 -0400

    for loop logic example

---
+++ Codes3/code14.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: basant
+ * Date: 2/23/2018
+ * Time: 3:44 PM
+ */
+for($i = 150; $i <= 500; $i ++){
+    if($i % 6 == 0){
+        print($i);
+        print"<br>";
+    }
+}
