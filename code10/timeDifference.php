7cc783b7205735342c007b7cc16272abdc4e7659
basantpatidar <basantpatidar33@gmail.com>
2018-08-02 23:21:54 -0400

    Calculatin time og called API

--- code10/timeDifference.php
+++ code10/timeDifference.php
@@ -8,11 +8,8 @@
 
 $oldTime = round(microtime(true),3);
 //used loop to make program slow and get time difference of around 1 Second
-$j = 1;
-for($i = 0; $i < 50000000; $i++ ){
-  $j ++;
+$homepage = file_get_contents('https://cyber-risk.upguard.com/api/cloudscan/details/v2/?hostname=arusuvaica.com&apikey=B8E2538B5221195ECA3D63A9453C2A7E&secret=262C06FF063FEB1790B8DAA497E209C5');
 
-}
 $newTime = round(microtime(true),3);
 $totalTime = $newTime - $oldTime;
 
@@ -20,3 +17,10 @@ echo "Old Time : ".$oldTime."<br>";
 echo "New Time : ".$newTime."<br>";
 echo "Calculated Time in Seconds up to 3 Decimals :".$totalTime."<br>";
 echo "Converted into Milliseconds : ".round($totalTime * 1000 );
+
+
+//On Console Printing
+echo "<script>console.log(\"Old Time : ".$oldTime."\")</script>";
+echo "<script>console.log(\"New Time : ".$newTime."\")</script>";
+echo "<script>console.log(\"Calculated Time in Seconds up to 3 Decimals : ".$totalTime."\")</script>";
+echo "<script>console.log(\"Converted into Milliseconds : ".$totalTime * 1000 ."\")</script>";
