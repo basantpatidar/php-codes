5096deec14d598a98e94e4c84b69ae860301b190
basantpatidar <basantpatidar33@gmail.com>
2018-08-08 23:28:34 -0400

    date month year

---
+++ Codes3/code17.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: basant
+ * Date: 2/23/2018
+ * Time: 4:45 PM
+ */
+echo date("Y/m/d") . "<br>";
+echo date("y.m.d") . "<br>";
+echo date("m-d-Y") . "<br>";
