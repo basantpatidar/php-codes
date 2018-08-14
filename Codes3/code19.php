0aa8170d7361328714f36f460b86b67a4d20b2b5
basantpatidar <basantpatidar33@gmail.com>
2018-08-14 17:42:42 -0400

    date checker

---
+++ Codes3/code19.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Date Validate</title>
+</head>
+<body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: basant
+ * Date: 2/20/2018
+ * Time: 2:20 PM
+ */
+print(var_dump(checkdate(2,28,2018)));
+?>
+</body>
+</html>
