e7dd2ec943822e471a70b4eb2bb0366deb7fc4ff
basantpatidar <basantpatidar33@gmail.com>
2018-08-25 20:52:27 -0400

    Nested For Loop

---
+++ Codes3/code25.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: basant
+ * Date: 2/24/2018
+ * Time: 1:07 AM
+ */
+
+$l =5;
+for($i = 0; $i < 11; $i ++) {
+    print"*";
+    if ($i < 5) {
+        for ($j = 0; $j < $i; $j++) {
+            print"*";
+        }
+    }
+
+    if ($i > 4) {
+        for ($k = $l; $k > 0; $k--) {
+            print"*";
+
+           /*print"$k";*/
+        }
+        $l--;
+
+    }/* print"$l";*/
+    print "<br>";
+}
