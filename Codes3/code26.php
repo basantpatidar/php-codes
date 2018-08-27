95899463ac7b0c1bf44c26855060550187c8cdf3
basantpatidar <basantpatidar33@gmail.com>
2018-08-26 21:16:58 -0400

    Operations on String

---
+++ Codes3/code26.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: basant
+ * Date: 2/25/2018
+ * Time: 2:16 AM
+ */
+$name = 'basant patidaR ';
+echo "Original String is : "."$name";
+echo "<br>";
+echo "transform a string all uppercase letters : ".strtoupper("$name");
+echo "<br>";
+echo " transform a string all lowercase letters : ".strtolower("$name");
+echo "<br>";
+echo "make a string's first character uppercase : ".ucfirst("$name");
+echo "<br>";
+echo "make a string's first character of all the words uppercase : ".ucwords("$name");
