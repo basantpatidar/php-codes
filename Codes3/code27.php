a160f4dc87cd342e2cec1d27db7c8281b414a464
basantpatidar <basantpatidar33@gmail.com>
2018-08-27 22:25:09 -0400

    Code27

---
+++ Codes3/code27.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: basant
+ * Date: 2/26/2018
+ * Time: 1:20 AM
+ */
+for($pqr = 0;$pqr < 10; $pqr ++){
+    print"pqr ";
+} print"<br>";
+for($asdf = 0;$asdf < 10; $asdf ++){
+    print"asdf ";
+}
+print"<br>";
+for($count = 1;$count < 16; $count ++){
+    print"$count"." ";
+}
+print"<br>";
+$i = 1;
+for ($x = ord('A'); $x <= ord('F'); $x++) {
+
+    echo $i.". Block ".chr($x)."<br>";
+    $i++;
+
+}
