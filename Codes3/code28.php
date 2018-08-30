1b413d54e730d5f31a18a7d74ef3c302c7f062c9
basantpatidar <basantpatidar33@gmail.com>
2018-08-29 23:46:51 -0400

    table print

---
+++ Codes3/code28.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: basant
+ * Date: 2/26/2018
+ * Time: 1:47 AM
+ */
+for($i = 1; $i < 100; $i ++){
+    $j = $i*$i;
+
+    print"$i * $i = ".$j;
+    print"<br>";
+
+}
