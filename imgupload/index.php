e8f05b56cc0be716cd077e58bdc7fe1239844ae0
basantpatidar <basantpatidar33@gmail.com>
2019-02-19 21:53:50 -0500

    index

---
+++ imgupload/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: basant
+ * Date: 4/26/2018
+ * Time: 1:53 AM
+ */?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+
+</head>
+<body>
+<form action="processImg.php" method="post" enctype="multipart/form-data">
+<input type="file" name="file">
+    <input type="submit" name="submit" value="Upload">
+
+</form>
+</body>
+</html>
